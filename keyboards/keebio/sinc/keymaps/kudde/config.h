517ee7e296baf8fd993efff9f196e058ef51d751
SYRE123 <malmsten.marcus@gmail.com>
2024-01-08 20:37:18 +0100

    Update config.h

--- keyboards/keebio/sinc/keymaps/kudde/config.h
+++ keyboards/keebio/sinc/keymaps/kudde/config.h
@@ -32,6 +32,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 150
 #define RGBLIGHT_SLEEP
 #define COMBO_TERM 25
-#define ENCODER_RESOLUTION 6
+#define ENCODER_RESOLUTION 4
 #define DEBOUNCE 25 //default is 5
 //#define COMBO_COUNT 3
