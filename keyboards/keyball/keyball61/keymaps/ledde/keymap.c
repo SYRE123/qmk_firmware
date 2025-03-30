7784a79b2f28976fe0f36f7850239f7ffb75e737
SYRE123 <malmsten.marcus@gmail.com>
2025-03-30 23:01:38 +0200

    Update keymap.c

--- keyboards/keyball/keyball61/keymaps/ledde/keymap.c
+++ keyboards/keyball/keyball61/keymaps/ledde/keymap.c
@@ -77,10 +77,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_universal(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    _______  , _______  , RALT(KC_X),DRAG_SCROLL, KC_BTN1  , _______  ,                                  _______  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  , KC_F12   ,
-    _______  , KC_BTN2  , KC_4     , KC_BTN3  , S(KC_BTN3) ,S(KC_SCLN),                                  KC_PGUP  , KC_BTN1  , DRAG_SCROLL  , KC_BTN2  , KC_BTN3  , NK_TOGG  ,
-    _______  , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , KC_PGDN  , _______  , _______  , _______  ,  CM_OFF  ,  CM_ON   ,
-    _______  , _______  , KC_0     , KC_DOT   , _______  , _______  , _______  ,             KC_DEL  , _______  , _______  , _______  , _______  , _______  , _______
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  , KC_F12   ,
+    _______  , KC_BTN2  , RALT(KC_X),DRAG_SCROLL, KC_BTN1  , KC_PGUP ,                                 KC_PGUP  , KC_BTN1  , DRAG_SCROLL  , KC_BTN2  , KC_BTN3  , NK_TOGG  ,
+    _______  , KC_BTN2  , _______  , KC_BTN3  , S(KC_BTN3) ,KC_PGDN , _______,              S(KC_9)  , KC_PGDN  ,S(KC_BTN3), _______  , _______  ,  CM_OFF  ,  CM_ON   ,
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,             KC_DEL  , _______  , _______  , _______  , _______  , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
