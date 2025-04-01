b32f54af6b339b800e1a17bfeb8a932db6405598
SYRE123 <malmsten.marcus@gmail.com>
2025-04-01 02:00:51 +0200

    Update config.h

--- keyboards/keyball/keyball61/keymaps/ledde/config.h
+++ keyboards/keyball/keyball61/keymaps/ledde/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 
-#define PMW33XX_CPI 600
+#define PMW33XX_CPI 700
 #define COMBO_TERM 25
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
